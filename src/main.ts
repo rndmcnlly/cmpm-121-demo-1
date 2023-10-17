1a720d4be4190af9d84b0f82fd923ea07176c043
Abel Goy <agoy@ucsc.edu>
2023-10-17 14:00:15 -0700

    Updated TS text, removed html extra

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,20 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const divHead = "Wow what a great fun";
+
+const div = document.createElement("div");
+div.innerHTML = divHead;
+app.append(div);
+
+/*
+const buttonName = "Click for cookies"
+
+document.body.innerHTML = buttonName;
+
+const header2 = document.createElement("h2");
+header2.innerHTML = buttonName;
+app.append(header2);
+*/
+//Click for cookies 🍪
