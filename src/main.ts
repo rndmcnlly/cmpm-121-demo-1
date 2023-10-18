a077c0b4ce3a1871e6a3d2defcc735bd4599710e
Abel Goy <agoy@ucsc.edu>
2023-10-17 20:52:19 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,45 +2,31 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
+// Game name
 const gameName = "Wow what a great Game";
-
 document.title = gameName;
-
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const divHead = "Wow what a great fun";
-
+let counter = 0;
+// Slightly smaller title text
+let divHead = `Wow what a great fun ${counter}` ;
 const div = document.createElement("div");
 div.innerHTML = divHead;
 app.append(div);
 
+// Button to farm🧅ONIONS
 const button = "🧅Click!"
 const mainButton = document.createElement("button")
 mainButton.innerHTML = button;
 app.append(mainButton);
 
-const abelEvent = new Event("abelEvent", {});
 
 // Help from TA Nicholas Nolasco
 mainButton.addEventListener("click", () => {
-  //let lines = [];
-  mainButton.dispatchEvent(abelEvent);
+  counter++;
+  divHead = `Wow what a great fun ${counter}` ;
+  div.innerHTML = divHead
 });
 
-/*mainButton.addEventListener("abelEvent", () => {
-  console.log("Abel says hello!);
-});*/
-
-/*
-const buttonName = "Click for cookies"
-
-document.body.innerHTML = buttonName;
-
-const header2 = document.createElement("h2");
-header2.innerHTML = buttonName;
-app.append(header2);
-*/
-//Click for cookies 🍪
-// 🧅ONIONS
