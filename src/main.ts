a61825641ff9a4df61848bfe417359e4cd8cceb3
Abel Goy <agoy@ucsc.edu>
2023-10-18 11:33:35 -0700

    Step 5 Complete

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,13 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-// Initiate counter
+// Initiate counter, growth rate, knives
 let counter = 0;
+let onionPerSec = 1;
+let knives = 0;
 
 // Slightly smaller title text
-let divHead = `The score is ${counter}` ;
+let divHead = `The score is ${counter}, with growth rate ${onionPerSec}` ;
 const div = document.createElement("div");
 div.innerHTML = divHead;
 app.append(div);
@@ -24,22 +26,37 @@ const mainButton = document.createElement("button")
 mainButton.innerHTML = button;
 app.append(mainButton);
 
+// Button to increase growth rate of farming onions
+let knife = `🔪Slice!\n(Cost 10) Knives: ${knives}`;
+const sideButton = document.createElement("button");
+sideButton.innerHTML = knife;
+app.append(sideButton);
+
+setInterval(() => {
+  if (counter >= 10) {
+    sideButton.disabled = false;
+  } else {
+    sideButton.disabled = true;
+  }
+});
 
 // Help from TA Nicholas Nolasco
 mainButton.addEventListener("click", () => {
   counter++;
-  divHead = `The score is ${counter}`;
+  divHead = `The score is ${counter}, with growth rate ${onionPerSec}`;
   div.innerHTML = divHead
 });
 
-/*
-// Auto clicking +1 (setInterval Imp for step 3)
-setInterval(function() {counter += 1}, 1000);
-setInterval(function() {div.innerHTML = divHead}, 1000);
-setInterval(function() {divHead = `The score is ${counter}`}, 1000);
-*/
+sideButton.addEventListener("click", () => {
+  counter -= 10;
+  onionPerSec += 0.2;
+  knives++;
+  divHead = `The score is ${counter}, with growth rate ${onionPerSec}`;
+  knife = `🔪Slice!\n(Cost 10) Knives: ${knives}`;
+  div.innerHTML = divHead
+  sideButton.innerHTML = knife;
+});
 
-const onionPerSec = 1;
 autoOnionClicking();
 
 function autoOnionClicking() {
@@ -50,7 +67,7 @@ function autoOnionClicking() {
     if (performance.now() - prevFrameTime > milPerFrame) {
       counter += 1;
       // div.textContent = `The score is ${counter.toPrecision(2)}` // if precise wanted
-      div.textContent = `The score is ${counter}`
+      div.textContent = `The score is ${counter}, with growth rate ${onionPerSec}`
       prevFrameTime = performance.now();
     } 
     window.requestAnimationFrame(checkCounterUpdate);
