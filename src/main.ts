c04758a24c76cb3e418d109ab07c537811357e98
Abel Goy <agoy@ucsc.edu>
2023-10-20 01:03:42 -0700

    Step 7 Complete (AND Step 6 Fix)

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,9 @@ let onionPerSec = 1;
 let knives = 0;
 let ninjas = 0;
 let robots = 0;
+let knifePrice = 10;
+let ninjaPrice = 100;
+let robotPrice = 1000;
 
 // Slightly smaller title text
 let divHead = `The score is ${counter}, with growth rate ${onionPerSec.toPrecision(2)}` ;
@@ -29,27 +32,27 @@ mainButton.innerHTML = button;
 app.append(mainButton);
 
 // Buttons to increase growth rate of farming onions
-let knife = `🔪Knife!\n(Cost 10) Knives: ${knives}`;
+let knife = `🔪Knife!\n(Cost ${knifePrice}) Knives: ${knives}`;
 const sideButton = document.createElement("button");
 sideButton.innerHTML = knife;
 app.append(sideButton);
 
-let ninja = `🥷🏼Ninja!!\n(Cost 100) Ninjas: ${ninjas}`;
+let ninja = `🥷🏼Ninja!!\n(Cost ${ninjaPrice}) Ninjas: ${ninjas}`;
 const sideButton2 = document.createElement("button");
 sideButton2.innerHTML = ninja;
 app.append(sideButton2);
 
-let robot = `🤖ROBOT!!!\n(Cost 1000) Robots: ${robots}`;
+let robot = `🤖ROBOT!!!\n(Cost ${robotPrice}) Robots: ${robots}`;
 const sideButton3 = document.createElement("button");
 sideButton3.innerHTML = robot;
 app.append(sideButton3);
 
 setInterval(() => {
-  if (counter >= 1000) {
+  if (counter >= robotPrice){//robotPrice) {
     sideButton3.disabled = false; 
-  } else if (counter >= 100) {
+  } else if (counter >= ninjaPrice) {
     sideButton2.disabled = false;
-  } else if (counter >= 10) {
+  } else if (counter >= knifePrice) {
     sideButton.disabled = false;
   } else {
     sideButton.disabled = true;
@@ -61,44 +64,47 @@ setInterval(() => {
 // Listeners for button reactions (Help from TA Nicholas Nolasco)
 mainButton.addEventListener("click", () => {
   counter++;
-  divHead = `The score is ${counter}, with growth rate ${onionPerSec.toPrecision(2)}`;
+  divHead = `The score is ${counter.toPrecision(4)}, with growth rate ${onionPerSec.toPrecision(2)}`;
   div.innerHTML = divHead
 });
 
 sideButton.addEventListener("click", () => {
-  counter -= 10;
-  onionPerSec += 0.2;
+  counter -= knifePrice;
+  knifePrice *= 1.15;
+  onionPerSec += 0.1;
   knives++;
-  divHead = `The score is ${counter}, with growth rate ${onionPerSec.toPrecision(2)}`;
-  knife = `🔪Knife!\n(Cost 10) Knives: ${knives}`;
+  divHead = `The score is ${counter.toPrecision(4)}, with growth rate ${onionPerSec.toPrecision(2)}`;
+  knife = `🔪Knife!\n(Cost ${knifePrice.toPrecision(4)}) Knives: ${knives}`;
   div.innerHTML = divHead;
   sideButton.innerHTML = knife;
 });
 
 sideButton2.addEventListener("click", () => {
-  counter -= 100;
+  counter -= ninjaPrice;
+  ninjaPrice *= 1.15;
   if (counter < 100) {
     sideButton2.disabled = true;
   }
-  onionPerSec += 1;
+  onionPerSec += 2;
   ninjas++;
-  divHead = `The score is ${counter}, with growth rate ${onionPerSec.toPrecision(2)}`;
-  ninja = `🥷🏼Ninja!!\n(Cost 100) Ninjas: ${ninjas}`;
+  divHead = `The score is ${counter.toPrecision(4)}, with growth rate ${onionPerSec.toPrecision(2)}`;
+  ninja = `🥷🏼Ninja!!\n(Cost ${ninjaPrice.toPrecision(4)}) Ninjas: ${ninjas}`;
   div.innerHTML = divHead;
   sideButton2.innerHTML = ninja;
 });
 
 sideButton3.addEventListener("click", () => {
-  counter -= 1000;
+  counter -= robotPrice;
+  robotPrice *= 1.15;
   if (counter < 1000) {
     sideButton3.disabled = true;
   }
-  onionPerSec += 10;
+  onionPerSec += 50;
   robots++;
-  divHead = `The score is ${counter}, with growth rate ${onionPerSec}`;
-  robot = `🤖ROBOT!!!\n(Cost 1000) Robots: ${robots}`;
+  divHead = `The score is ${counter.toPrecision(4)}, with growth rate ${onionPerSec}`;
+  robot = `🤖ROBOT!!!\n(Cost ${robotPrice.toPrecision(4)}) Robots: ${robots}`;
   div.innerHTML = divHead
-  sideButton.innerHTML = robot;
+  sideButton3.innerHTML = robot;
 });
 
 autoOnionClicking();
@@ -110,7 +116,7 @@ function autoOnionClicking() {
     const milPerFrame = 1000 / onionPerSec;
     if (performance.now() - prevFrameTime > milPerFrame) {
       counter += 1;
-      div.textContent = `The score is ${counter}, with growth rate ${onionPerSec.toPrecision(2)}`
+      div.textContent = `The score is ${counter.toPrecision(4)}, with growth rate ${onionPerSec.toPrecision(2)}`
       prevFrameTime = performance.now();
     } 
     window.requestAnimationFrame(checkCounterUpdate);
